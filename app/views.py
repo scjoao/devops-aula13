ee3869edd458d5c8d6fc86d394104a98de3a7038
scjoao <joao.carlos_25@hotmail.com>
2017-11-08 21:21:03 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
-from app.models import Curso, Vestibular
+from app.models import Curso, Vestibular, Candidato
 from datetime import datetime
 
 def home(request):
@@ -84,9 +84,8 @@ def lista_candidatos(request):
         'app/lista_candidatos.html',
         context_instance = RequestContext(request,
         {
-            'title':'Lista de candidatos',
-            'vestibulares': Candidato.objects.all(),
+            'title':'Cadastro de candidatos',
+            'candidatos': Candidato.objects.all(),
             'year':datetime.now().year,
         })
     )
-
